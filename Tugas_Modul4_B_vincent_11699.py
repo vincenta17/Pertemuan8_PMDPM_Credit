e7ec72728c88e8ba2a5f46a9d067c280f1256a79
Vincent Alexander Li <151655134+vincenta17@users.noreply.github.com>
2024-11-06 15:40:45 +0700

    Update Tugas_Modul4_B_vincent_11699.py

--- Tugas_Modul4_B_vincent_11699.py
+++ Tugas_Modul4_B_vincent_11699.py
@@ -48,11 +48,10 @@ if uploaded_file is not None:
     st.markdown("<h1 style='text-align: center;'>Unsupervised Learning - 11699</h1>", unsafe_allow_html=True) 
     st.dataframe(input_data)
 
-model_directory = r'D:\Semester 5\Pembelajaran Mesin Dan Mendalam\module 4\Tugas4_B_11699'
 model_path = {
-    "AGG_model": os.path.join(model_directory, r"AGG_model.pkl"),
-    "KMeans_model": os.path.join(model_directory, r"KMeans_model.pkl"),
-    "DBSCAN_model": os.path.join(model_directory, r"DBSCAN_model.pkl"),
+    "AGG_model":  r"AGG_model.pkl",
+    "KMeans_model": r"KMeans_model.pkl",
+    "DBSCAN_model": r"DBSCAN_model.pkl",
 }
 
 models = {}
